1b5e0491dfdc8dbaf6e81f60be860ca5d26fc0f3
deybyr647 <deybyr647@gmail.com>
2020-08-06 23:50:53 -0400

    Began adding form functionality

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 let weatherContainer = document.querySelector('.weather-container');
 let body = document.querySelector('body');
-let owm = 'https://api.openweathermap.org/data/2.5/weather?zip=11722,us&appid=adb117b6500a18ac7ad60e3d23b0ac24&lang=en&units=imperial'
+let owm = `https://api.openweathermap.org/data/2.5/weather?zip=11722,us&appid=adb117b6500a18ac7ad60e3d23b0ac24&lang=en&units=imperial`;
 
 fetch(owm)
     .then((response) => (
