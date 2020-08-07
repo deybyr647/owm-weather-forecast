485030c20c2ea03206679d7a395fb05bb02f4314
deybyr647 <deybyr647@gmail.com>
2020-08-06 23:39:23 -0400

    Began adding CSS styling, and clean weather information

--- weather.js
+++ weather.js
@@ -12,6 +12,15 @@ fetch(owm)
         currentWeather(weatherObj);
     })
 
+let capitalizeStr = (str) => {
+    let splitStr = str.toLowerCase().split(' ');
+    for(let l = 0; l < splitStr.length; l++){
+        splitStr[l] = splitStr[l].charAt(0).toUpperCase() + splitStr[l].substring(1);
+    }
+
+    return splitStr.join(' ');
+
+}
 
 let currentWeather = (weatherObj) => {
     let city = document.querySelector('.city');
@@ -24,15 +33,29 @@ let currentWeather = (weatherObj) => {
     weatherIcon.src = `http://openweathermap.org/img/wn/${weatherObj.weather[0].icon}@2x.png`;
     weatherCard.appendChild(weatherIcon);
 
+    let weatherDescription = document.createElement('p');
+    weatherDescription.innerHTML = capitalizeStr(weatherObj.weather[0].description);
+    weatherCard.appendChild(weatherDescription);
+
     let mainWeather = document.createElement('p');
-    mainWeather.innerHTML = weatherObj.main.temp;
+    mainWeather.innerHTML = `${Math.floor(weatherObj.main.temp)} &deg`;
     weatherCard.appendChild(mainWeather);
 
     let feelsLike = document.createElement('p');
-    feelsLike.innerHTML = `Feels Like ${weatherObj.main.feels_like}`;
+    feelsLike.innerHTML = `Feels Like ${Math.ceil(weatherObj.main.feels_like)} &deg`;
     weatherCard.appendChild(feelsLike);
 
+    let windSpeed = document.createElement('p');
+    windSpeed.innerHTML = `Wind: ${Math.floor(weatherObj.wind.speed)} MPH`;
+    weatherCard.appendChild(windSpeed);
 
+    let pressure = document.createElement('p');
+    pressure.innerHTML = `Pressure: ${Math.ceil(weatherObj.main.pressure)} hPa`;
+    weatherCard.appendChild(pressure);
+
+    let humidityPercentage = document.createElement('p');
+    humidityPercentage.innerHTML = `Humidity: ${Math.ceil(weatherObj.main.humidity)} &#37`;
+    weatherCard.appendChild(humidityPercentage);
 
     weatherContainer.appendChild(weatherCard);
 }
